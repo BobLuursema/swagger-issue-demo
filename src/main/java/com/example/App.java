c2110520c4ebeeb7298508e120b915f99dbabfd2
Bob Luursema <bob.luursema@nn.nl>
2019-12-20 10:25:05 +0100

    Fix version and dependency

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,10 +2,6 @@ package com.example;
 
 import io.swagger.v3.core.converter.ModelConverters;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {
         var schema = ModelConverters.getInstance().read(Event.class).get("Event");
